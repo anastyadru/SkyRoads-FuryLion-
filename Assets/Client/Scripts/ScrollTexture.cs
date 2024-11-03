6c9fcdd60435635d1ba0a2928e25d78c670ad309
Nastia <adruts@mail.ru>
2024-11-03 20:44:35 +0300

    realization-road changes
    implemented the endless work of the road

---
+++ Assets/Client/Scripts/ScrollTexture.cs
@@ -0,0 +1,20 @@
+// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
+
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+    private float offset;
+    private Material myMaterial;
+    
+    void Awake()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        offset -= 12f * Time.deltaTime;
+        myMaterial.mainTextureOffset = new Vector2(0, offset);
+    }
+}
